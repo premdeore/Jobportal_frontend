f7b1bfcbcce28031d356a8b80526865f747bda1a
premdeore <prem.developer69@gmail.com>
2024-10-10 14:22:59 +0530

    frontend setup with basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}', // Adjust according to your project's file extensions
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
+
+
+
+
+
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
